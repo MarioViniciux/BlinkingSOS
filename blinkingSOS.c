e8e176346ed7a80b725a8b3412711fbe43d14e5d
Mario Vinicius <marioviniciuxcs@gmail.com>
2025-01-11 21:11:28 -0300

    :docs::comments: Adição de comentários em cada bloco de código para melhor compreensão

--- blinkingSOS.c
+++ blinkingSOS.c
@@ -1,22 +1,25 @@
 #include "pico/stdlib.h"
 
+// Define o tempo para ponto, traço e espaçamento (em milissegundos)
 #define DOT_DURATION 200
 #define DASH_DURATION 800
 #define LETTER_SPACE 250
 #define SYMBOL_SPACE 125
 
+// Função para acionar o LED
 void morse_led(int pin, bool state) {
     gpio_put(pin, state);
 }
 
+// Função para trasnformar a letra em código morse (no caso, somente S e O)
 void send_morse(char letter, int pin) {
     switch (letter) {
-        case 'S': 
+        case 'S': // Em morse, S é representado por "..."
             morse_led(pin, true); sleep_ms(DOT_DURATION); morse_led(pin, false); sleep_ms(SYMBOL_SPACE);
             morse_led(pin, true); sleep_ms(DOT_DURATION); morse_led(pin, false); sleep_ms(SYMBOL_SPACE);
             morse_led(pin, true); sleep_ms(DOT_DURATION); morse_led(pin, false); sleep_ms(LETTER_SPACE);
             break;
-        case 'O': 
+        case 'O': // Em morse, O é representado por "---"
             morse_led(pin, true); sleep_ms(DASH_DURATION); morse_led(pin, false); sleep_ms(SYMBOL_SPACE);
             morse_led(pin, true); sleep_ms(DASH_DURATION); morse_led(pin, false); sleep_ms(SYMBOL_SPACE);
             morse_led(pin, true); sleep_ms(DASH_DURATION); morse_led(pin, false); sleep_ms(LETTER_SPACE);
@@ -25,14 +28,17 @@ void send_morse(char letter, int pin) {
 }
 
 int main() {
-    gpio_init(13);
-    gpio_set_dir(13, GPIO_OUT);
+    // Inicia o sistema GPIO
+    gpio_init(13); // Usando o pino 13
+    gpio_set_dir(13, GPIO_OUT); 
 
     while (true) {
+        // Chama a função criada acima, para passar as letras para o código morse
         send_morse('S', 13);
         send_morse('O', 13);
         send_morse('S', 13);
 
+        // Aguarda 3 segundos antes de repetir
         sleep_ms(3000);
     }
     return 0;
